057bc50ca8b2206955cd47e2549f74dde5e7300e
Steven Weber <webersteven93@gmail.com>
2014-06-13 18:38:53 +0200

    Erste dynamische Controls (Buttons) LCD Display

--- SortView.cpp
+++ SortView.cpp
@@ -3,17 +3,27 @@
 
 SortView::SortView(QWidget *parent, int* startTuple, int numbOfValues) :
     QMainWindow(parent),
-    ui(new Ui::SortView)
-{
+    ui(new Ui::SortView){
+    ui->setupUi(this);
     this->startTuple = new int[numbOfValues];
+
     for (int i = 0; i < numbOfValues; i++)
     {
         this->startTuple[i] = startTuple[i];
+        QLabel *btn = new QLabel();
+        //QRect *rect = new QRect();
+        btn->setMinimumHeight(startTuple[i]*10);
+        //*rect->setMinimumHeight(startTuple[i]*10);
+        ui->horLayoutSorting->addWidget(btn);
     }
-    ui->setupUi(this);
     this->startTuple = startTuple;
     this->numbOfValues = numbOfValues;
     controller = new Controller(startTuple, numbOfValues);
+
+
+
+
+
 }
 
 SortView::~SortView()
@@ -25,11 +35,12 @@ void SortView::on_btnNextStep_clicked()
 {
     controller->getBubbleSortCtrl()->setNextStep();
     ui->lblExplanation->setText(QString::fromStdString(controller->getBubbleSortCtrl()->getCurrentStep()->toString()));
-
+    ui->lcdNumActStep->display(controller->getBubbleSortCtrl()->getCurrentStep()->getNumber());
 }
 
 void SortView::on_btnPrevStep_clicked()
 {
     controller->getBubbleSortCtrl()->setPrevStep();
     ui->lblExplanation->setText(QString::fromStdString(controller->getBubbleSortCtrl()->getCurrentStep()->toString()));
+    ui->lcdNumActStep->display(controller->getBubbleSortCtrl()->getCurrentStep()->getNumber());
 }
