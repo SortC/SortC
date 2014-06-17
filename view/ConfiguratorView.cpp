9ef719870fa0059877e428c55a155e25853c27af
stsander <Sander_Stefanie@gmx.net>
2014-06-17 18:34:48 +0200

    Dummy clickevent angelegt

--- view/ConfiguratorView.cpp
+++ view/ConfiguratorView.cpp
@@ -1,5 +1,6 @@
 #include "ConfiguratorView.h"
 #include "ui_ConfiguratorView.h"
+#include "SortView.h"
 
 ConfiguratorView::ConfiguratorView(QWidget *parent) :
     QMainWindow(parent),
@@ -8,7 +9,16 @@ ConfiguratorView::ConfiguratorView(QWidget *parent) :
     ui->setupUi(this);
 }
 
+
+
 ConfiguratorView::~ConfiguratorView()
 {
     delete ui;
+}
+
+void ConfiguratorView::on_startBtn_clicked()
+{
+    //qDebug("Value1 = %.4f", ui->valueSpinBox->value());
+    //SortView* sortView = new SortView(0, 0 , ui->valueSpinBox->value());
+    //sortView->show();
 }
--- view/ConfiguratorView.h
+++ view/ConfiguratorView.h
@@ -15,6 +15,9 @@ public:
     explicit ConfiguratorView(QWidget *parent = 0);
     ~ConfiguratorView();
 
+private slots:
+    void on_startBtn_clicked();
+
 private:
     Ui::ConfiguratorView *ui;
 };
