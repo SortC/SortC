7aa3e5a2c1317290a8f0c674e40c7e47efebb86b
Steven Weber <webersteven93@gmail.com>
2014-06-19 19:45:49 +0200

    Kommentierung der Controllerklassen

--- controller/AlgorithmController.h
+++ controller/AlgorithmController.h
@@ -2,7 +2,12 @@
 #define ALGORITHMCONTROLLER_H
 
 #include "model/Algorithm.h"
-
+/**
+ * @brief The AlgorithmController class
+ * Diese Klasse abstrahiert die Funktionalität eines von Algorithm
+ * abgeleiteten Sortieralgorithmus. Damit trennt sie die Visualisierung
+ * von der Logik.
+ */
 class AlgorithmController
 {
 public:
--- controller/Controller.h
+++ controller/Controller.h
@@ -11,13 +11,33 @@
 #include "model/BucketSort.h"
 #include <cstdlib>
 #include <time.h>
-
+/**
+ * @brief The Controller class
+ * Diese Klasse stellt alle Sortierverfahren zur Verfügung. Dabei ist sie lediglich
+ * ein Container für mehrere AlgorithmController Objekte. Durch die Container-Funktion
+ * kann von außen über jedes registrierte Sortierverfahren verfügt werden ohne einzelne
+ * Controller-Klassen instanziieren zu müssen
+ */
 class Controller
 {
 public:
+    /**
+     * @brief Controller Konstruktor analog zu Algorithm für vordefiniertes Zahlentupel
+     * @param startTuple   vordefiniertes Zahlentupel
+     * @param numbOfValues Anzahl der Werte
+     */
     Controller(int *startTuple, int numbOfValues);
+
+    /**
+     * @brief Controller Konstruktor analog zu Algorithm für zufälliges Zahlentupel
+     * @param numbOfValues Anzahl der Werte
+     */
     Controller(int numbOfValues);
 
+    /**
+     * @brief get*SortCtrl
+     * Getter-Methoden der einzelnen AlgorithmController
+     */
     AlgorithmController* getBubbleSortCtrl() const {return bubbleSortCtrl;}
     AlgorithmController* getSelectionSortCtrl() const{return selectionSortCtrl;}
     AlgorithmController* getInsertionSortCtrl() const{return insertionSortCtrl;}
