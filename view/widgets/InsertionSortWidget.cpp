78ee4446159ee348ea61384789f6df338a680d0d
SockeThe2nd <roland.paltz@gmx.net>
2014-06-21 14:46:10 +0200

    InsertionSort Implementiert

--- view/widgets/InsertionSortWidget.cpp
+++ view/widgets/InsertionSortWidget.cpp
@@ -6,8 +6,14 @@ InsertionSortWidget::InsertionSortWidget(QWidget *parent, AlgorithmController* a
     displayValues = new SortValue*[numbOfValues];
     SortValue* newValue;
 
+    int max = 0;
     for (int i = 0; i < numbOfValues; i++){
-        newValue = new SortValue(0,startTuple[i]);
+        if(max < startTuple[i])
+            max = startTuple[i];
+    }
+
+    for (int i = 0; i < numbOfValues; i++){
+        newValue = new SortValue(0,startTuple[i], max);
         ui->horLayoutSorting->addWidget(newValue,0,Qt::AlignBottom);
         displayValues[i] = newValue;
     }
@@ -21,4 +27,33 @@ InsertionSortWidget::InsertionSortWidget(QWidget *parent, AlgorithmController* a
 void InsertionSortWidget::handleStep()
 {
     BaseSortWidget::handleStep();
+
+    for(int i = 0; i < numbOfValues; i++) {
+        displayValues[i]->setAction(SortValue::NONE);
+    }
+
+    int val1, val2, buffer;
+
+    val1 = algoCtrl->getCurrentStep()->getFirstValue();
+    val2 = algoCtrl->getCurrentStep()->getSecondValue();
+
+    switch (algoCtrl->getCurrentStep()->getOperation()) {
+    case Step::COMP:
+        displayValues[val1]->setAction(SortValue::COMP);
+        displayValues[val2]->setAction(SortValue::COMP);
+        break;
+    case Step::SWAP:
+        displayValues[val1]->setAction(SortValue::SWAP);
+        displayValues[val2]->setAction(SortValue::SWAP);
+        buffer = displayValues[val1]->getValue();
+        displayValues[val1]->setValue(displayValues[val2]->getValue());
+        displayValues[val2]->setValue(buffer);
+        break;
+    case Step::END:
+        ui->btnPlayPause->setChecked(false);
+        ui->btnNextStep->setEnabled(false);
+        ui->btnPlayPause->setEnabled(false);
+    default:
+        break;
+    }
 }
