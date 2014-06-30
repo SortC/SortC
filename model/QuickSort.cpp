ecd2028fef9241a1b7584fa3cbb19d21b30bf12e
Steven Weber <webersteven93@gmail.com>
2014-06-30 18:41:59 +0200

    QuickSort funzt, Schritte fehlen aber noch teilweise

--- model/QuickSort.cpp
+++ model/QuickSort.cpp
@@ -10,39 +10,122 @@ QuickSort::QuickSort(int numberOfValues) : Algorithm(numberOfValues)
 {
 }
 
-void QuickSort::sort(){
-	int left = 0;
-	int right = numbOfValues-1;
+//void QuickSort::sort(){
+//	int left = 0;
+//	int right = numbOfValues-1;
 
-	if(left >= right)
-		return; // Invalid index range
+//	if(left >= right)
+//		return; // Invalid index range
 
-	vector<std::pair<int, int> > list;
-	list.push_back(pair<int, int>(left, right));
+//	vector<std::pair<int, int> > list;
+//	list.push_back(pair<int, int>(left, right));
 
-	while(list.size() != 0)
-	{
-		left = list.back().first;
-		right = list.back().second;
-		list.pop_back();
+//	while(list.size() != 0)
+//	{
+//		left = list.back().first;
+//		right = list.back().second;
+//		list.pop_back();
 
-        steps.push_back(new Step(left,right,Step::MARK,++numbOfSteps));
-		int pivot = partition(currentTuple, left, right);   
-        steps.push_back(new Step(pivot,0,Step::PIVOT,++numbOfSteps));
+//        steps.push_back(new Step(left,right,Step::MARK,++numbOfSteps));
+//		int pivot = partition(currentTuple, left, right);
+//     //   steps.push_back(new Step(pivot,0,Step::PIVOT,++numbOfSteps));
 
-        //stringstream buffer;
-        //buffer << "Pruefe ob Pivot   [" << pivot <<"] > "<< 1 ;
+//        //stringstream buffer;
+//        //buffer << "Pruefe ob Pivot   [" << pivot <<"] > "<< 1 ;
 //        steps.push_back(new Step(pivot,1,Step::COMP, ++numbOfSteps/*, buffer.str()*/ ));
-		if(pivot > 1)
-			list.push_back(std::pair<int, int>(left, pivot - 1));
+//		if(pivot > 1)
+//			list.push_back(std::pair<int, int>(left, pivot - 1));
 
-        //buffer << "Pruefe ob Pivot   [" << pivot + 1 <<"] > "<< 1 ;
-  //      steps.push_back(new Step(pivot + 1, right ,Step::COMP, ++numbOfSteps/*, buffer.str()*/ ));
-		if(pivot + 1 < right)
-			list.push_back(std::pair<int, int>(pivot + 1, right));
-	}
-    steps.push_back(new Step(0,0,Step::END,++numbOfSteps));
-}
+//        //buffer << "Pruefe ob Pivot   [" << pivot + 1 <<"] > "<< 1 ;
+//  //      steps.push_back(new Step(pivot + 1, right ,Step::COMP, ++numbOfSteps/*, buffer.str()*/ ));
+//		if(pivot + 1 < right)
+//			list.push_back(std::pair<int, int>(pivot + 1, right));
+//	}
+//    steps.push_back(new Step(0,0,Step::END,++numbOfSteps));
+//}
+
+void QuickSort::sort(){
+         //System.Collections.Stack stack = new System.Collections.Stack();
+
+    int pivot;
+     std::stack<int> stack;
+         int pivotIndex = 0;
+         int leftIndex = pivotIndex + 1;
+         int rightIndex = this->numbOfValues - 1;
+
+
+         stack.push(pivotIndex);//Push always with left and right
+         stack.push(rightIndex);
+
+         int leftIndexOfSubSet, rightIndexOfSubset;
+
+         while (stack.size() > 0)
+         {
+
+             rightIndexOfSubset = stack.top() ;
+             stack.pop();//pop always with right and left
+             leftIndexOfSubSet = stack.top() ;
+             stack.pop();
+
+             leftIndex = leftIndexOfSubSet + 1;
+             pivotIndex = leftIndexOfSubSet;
+             rightIndex = rightIndexOfSubset;
+
+             pivot = this->currentTuple[pivotIndex];
+
+             if (leftIndex > rightIndex)
+                 continue;
+
+             while (leftIndex < rightIndex)
+             {
+                 steps.push_back(new Step(leftIndex, rightIndex,Step::COMP,++numbOfSteps, buffer.str() ));
+                 while ((leftIndex <= rightIndex) && (this->currentTuple[leftIndex] <= pivot))
+                     leftIndex++;	//increment right to find the greater
+             //element than the pivot
+
+                 while ((leftIndex <= rightIndex) && (this->currentTuple[rightIndex] >= pivot))
+                     rightIndex--;//decrement right to find the
+
+                 //smaller element than the pivot
+                 stringstream buffer;
+                 buffer << "Pruefe ob rechter Zeiger [" << rightIndex <<"] >= linker Zeiger ["<< leftIndex << "]" ;
+                 steps.push_back(new Step(rightIndex, leftIndex,Step::COMP,++numbOfSteps, buffer.str() ));
+                 if (rightIndex >= leftIndex) {  //if right index is
+                     //greater then only swap
+                     //SwapElement(ref input, leftIndex, rightIndex);
+                     steps.push_back(new Step(rightIndex, leftIndex,Step::SWAP,++numbOfSteps));
+                     int temp1 = currentTuple[leftIndex];
+                     currentTuple[leftIndex] = currentTuple[rightIndex];        // #
+                     currentTuple[rightIndex] = temp1;
+                 }
+             }
+
+             stringstream buffer2;
+             buffer2 << "Pruefe ob Pivot [" << pivotIndex <<"] > rechter Zeiger ["<< rightIndex << "]" ;
+             steps.push_back(new Step(pivotIndex, rightIndex,Step::COMP,++numbOfSteps, buffer2.str() ));
+             if (pivotIndex <= rightIndex)
+                 if( currentTuple[pivotIndex] > currentTuple[rightIndex]) {
+                     steps.push_back(new Step(pivotIndex, rightIndex,Step::SWAP,++numbOfSteps));
+                     int temp2 = this->currentTuple[pivotIndex];
+                     this->currentTuple[pivotIndex] = currentTuple[rightIndex];
+                     this->currentTuple[rightIndex] = temp2;
+                 }
+
+             if (leftIndexOfSubSet < rightIndex)
+             {
+                 stack.push(leftIndexOfSubSet);
+                 stack.push(rightIndex - 1);
+             }
+
+             if (rightIndexOfSubset > rightIndex)
+             {
+                 stack.push(rightIndex + 1);
+                 stack.push(rightIndexOfSubset);
+             }
+         }
+          steps.push_back(new Step(pivotIndex, rightIndex,Step::END,++numbOfSteps));
+     }
+
 
 int QuickSort::partition(int a[], int left, int right)
 {
@@ -55,20 +138,20 @@ int QuickSort::partition(int a[], int left, int right)
 		while (a[left] < pivot){
 			stringstream buffer;
             buffer << "Pruefe ob \tPivot[" << pivotStelle <<"]  < LZ[" << left << "]" ;
-            steps.push_back(new Step(pivotStelle, left,Step::COMP, ++numbOfSteps, buffer.str() ));
+            //steps.push_back(new Step(pivotStelle, left,Step::COMP, ++numbOfSteps, buffer.str() ));
 			left++;
 		}
 
 		while (a[right] > pivot){
 			stringstream buffer;
             buffer << "Pruefe ob \tPivot[" << pivotStelle <<"]  > RZ[" << right <<"]" ;
-            steps.push_back(new Step(pivotStelle, left,Step::COMP, ++numbOfSteps, buffer.str() ));
+           // steps.push_back(new Step(pivotStelle, left,Step::COMP, ++numbOfSteps, buffer.str() ));
 			right--;
 		}
 
         stringstream buffer;
         buffer << "Pruefe ob \tLZ   [" << left << "]  < RZ[" << right << "]" ;
-        steps.push_back(new Step(pivotStelle, left,Step::COMP, ++numbOfSteps, buffer.str() ));
+        //steps.push_back(new Step(pivotStelle, left,Step::COMP, ++numbOfSteps, buffer.str() ));
 
 		if (left < right)
 		{
--- model/QuickSort.h
+++ model/QuickSort.h
@@ -2,6 +2,7 @@
 #include "algorithm.h"
 #include <stack>
 
+
 class QuickSort :
 	public Algorithm
 {
