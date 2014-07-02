78ec1904690cc542bf0efb7f1414d69f52033b59
stsander <Sander_Stefanie@gmx.net>
2014-07-02 17:36:29 +0200

    MergeSortWidget Actions verbessert (nicht vollständig getestet)

--- view/widgets/MergeSortWidget.cpp
+++ view/widgets/MergeSortWidget.cpp
@@ -21,12 +21,12 @@ MergeSortWidget::MergeSortWidget(QWidget *parent, AlgorithmController* algoCtrl,
     }
 
     for (int i = 0; i < numbOfValues; i++){
-        tempValues = new SortValue(0, i, max, i);
+        tempValues = new SortValue(0, max, max, i);
         ui->horizontalLayout_4->addWidget(tempValues,0,Qt::AlignBottom);
 
         helpArray[i] = tempValues;
-        helpArray[i]->setMinimumHeight(180);
-        helpArray[i]->hideValue();
+        //helpArray[i]->setMaximumHeight(max);
+        helpArray[i]->setStyleSheet("background-color: white");
     }
 
     this->url = QString("http://de.wikipedia.org/wiki/Mergesort");
@@ -68,6 +68,8 @@ void MergeSortWidget::handleStep()
 
     switch (algoCtrl->getCurrentStep()->getOperation()) {
     case Step::COMP:
+        helpArray[val1]->setAction(SortValue::NONE);
+        helpArray[val2]->setAction(SortValue::NONE);
         displayValues[val1]->setAction(SortValue::COMP);
         displayValues[val2]->setAction(SortValue::COMP);
         break;
@@ -76,10 +78,11 @@ void MergeSortWidget::handleStep()
         displayValues[val2]->setAction(SortValue::NONE);
         if(directionForward){
             displayValues[val2]->setValue(helpArray[val1]->getValue());
-            helpArray[val1]->hideValue();
+            //helpArray[val1]->hideValue();
         } else {
             helpArray[val1]->setValue(displayValues[val2]->getValue());
-            helpArray[val1]->showValue();
+            //helpArray[val2]->setAction(SortValue::NONE);
+            //helpArray[val1]->showValue();
         }
         break;
     case Step::CPY:
@@ -87,14 +90,21 @@ void MergeSortWidget::handleStep()
         helpArray[val2]->setAction(SortValue::CPY);
         if(directionForward){
             helpArray[val2]->setValue(displayValues[val1]->getValue());
-            helpArray[val2]->showValue();
+            //helpArray[val2]->showValue();
         } else {
             displayValues[val1]->setValue(helpArray[val2]->getValue());
-            helpArray[val2]->hideValue();
+
         }
         break;
     case Step::MARK:
+
+        for (int j = 0; j < numbOfValues; j++)
+        {
+            helpArray[j]->setAction(SortValue::NONE);
+        }
+
         markedValues = val2-val1;
+
         for (int i = 0; i <= markedValues; i++)
         {
             displayValues[val1+i]->setAction(SortValue::MARK);
@@ -120,12 +130,21 @@ void MergeSortWidget::handleStep()
 void MergeSortWidget::on_btnReset_clicked(){
     BaseSortWidget::on_btnReset_clicked();
 
+    int max = 0;
+    for (int i = 0; i < numbOfValues; i++){
+        if(max < startTuple[i])
+            max = startTuple[i];
+    }
+
     for(int i = 0; i<numbOfValues; i++){
         displayValues[i]->setValue(startTuple[i]);
+        displayValues[i]->setAction(SortValue::NONE);
     }
-    for(int i = 0; i < numbOfValues; i++)
-    {
-        helpArray[i]->hideValue();
+
+    for(int i = 0; i < numbOfValues; i++){
+        helpArray[i]->setValue(max);
+        helpArray[i]->setStyleSheet("background-color: white");
+        //helpArray[i]->setAction(SortValue::NONE);
     }
     handleStep();
 }
